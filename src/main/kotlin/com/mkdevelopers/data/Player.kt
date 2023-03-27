4159aaad50f08284532d96cb287078c0329124ab
Mohan <jpmohan007@gmail.com>
2023-03-27 09:41:32 +0530

    implemented ping functionality

--- src/main/kotlin/com/mkdevelopers/data/Player.kt
+++ src/main/kotlin/com/mkdevelopers/data/Player.kt
@@ -1,6 +1,11 @@
 package com.mkdevelopers.data
 
+import com.mkdevelopers.data.models.Ping
+import com.mkdevelopers.gson
+import com.mkdevelopers.server
+import com.mkdevelopers.util.Constants
 import io.ktor.websocket.*
+import kotlinx.coroutines.*
 
 data class Player(
     val userName: String,
@@ -9,4 +14,43 @@ data class Player(
     var isDrawing: Boolean = false,
     var score: Int = 0,
     var rank: Int = 0
-)
+) {
+    private var pingJob: Job? = null
+
+    private var pingTime = 0L
+    private var pongTIme = 0L
+
+    var isOnline = true
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun startPinging() {
+        pingJob?.cancel()
+        pingJob = GlobalScope.launch {
+            while(true) {
+                sendPing()
+                delay(Constants.PING_FREQUENCY)
+            }
+        }
+    }
+
+    private suspend fun sendPing() {
+        pingTime = System.currentTimeMillis()
+        socket.send(Frame.Text(gson.toJson(Ping())))
+        delay(Constants.PING_FREQUENCY)
+
+        if(pingTime - pongTIme > Constants.PING_FREQUENCY) {
+            isOnline = false
+            server.playerLeft(clientId)
+            pingJob?.cancel()
+        }
+    }
+
+    fun receivedPong() {
+        pongTIme = System.currentTimeMillis()
+        isOnline = true
+    }
+
+    fun disconnect() {
+        pingJob?.cancel()
+    }
+}
---
+++ src/main/kotlin/com/mkdevelopers/data/models/Ping.kt
@@ -0,0 +1,5 @@
+package com.mkdevelopers.data.models
+
+import com.mkdevelopers.util.Constants
+
+class Ping : BaseModel(Constants.TYPE_PING)
